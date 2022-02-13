0f31f2f90dfffcf59a6e2df56296510e43f412ea
[AJ] <ajai.np@gmail.com>
2022-02-13 11:42:50 +1100

    pre delete hello component

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,17 +1,14 @@
-import React from 'react'
 import './Hello.css';
 
-class Hello extends React.Component<{ greetings: string, age: number }> {
+const Hello = (props: { greetings: string, age: number }) => {
 
+    return (
+        <div className="f1 tc">
+            <h1>Hello world</h1>
+            <p>Welcome to react {props.greetings} with age {props.age}</p>
+        </div>
+    );
 
-    render(): React.ReactNode {
-        return (
-            <div className="f1 tc">
-                <h1>Hello world</h1>
-                <p>Welcome to react {this.props.greetings}</p>
-            </div>
-        );
-    }
 }
 
 export default Hello;
